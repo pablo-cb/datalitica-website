0088bf54dcb1879a86a1ae43a0b5a0e49ba5d3f8
Pablo Calderon Bravo <pablocalderonbravo@gmail.com>
2023-05-28 14:37:09 -0600

    Services sections totally working

--- script.js
+++ script.js
@@ -94,86 +94,97 @@ servicesList.push({
 });
 
 
+
 function buildServicesHTML(list) {
-const servicesContainer = document.querySelector('.services__expand-cards');
-let activeCard = null;
+    const servicesContainer = document.querySelector('.services__expand-cards');
+    let activeCard = null;
+    list.forEach(service => {
+        // Create the main container
+        const expandCard = document.createElement('div');
+        expandCard.classList.add('services__expand-card');
 
-list.forEach(service => {
-    // Create the main container
-    const expandCard = document.createElement('div');
-    expandCard.classList.add('services__expand-card');
+        // Create the title container
+        const titleContainer = document.createElement('div');
+        titleContainer.classList.add('services__expand-card-title-container');
 
-    // Create the title container
-    const titleContainer = document.createElement('div');
-    titleContainer.classList.add('services__expand-card-title-container');
+        // Create the title element
+        const title = document.createElement('h4');
+        title.classList.add('services__expand-card-title', 'h4-global-font');
+        title.textContent = service.serviceName;
 
-    // Create the title element
-    const title = document.createElement('h4');
-    title.classList.add('services__expand-card-title', 'h4-global-font');
-    title.textContent = service.serviceName;
+        // Create the expand icon
+        const expandIcon = document.createElement('img');
+        expandIcon.classList.add('services__expand-card-expand-icon');
+        expandIcon.setAttribute('src', './public/services--expandcardplusicon.svg');
+        expandIcon.setAttribute('alt', '');
 
-    // Create the expand icon
-    const expandIcon = document.createElement('img');
-    expandIcon.classList.add('services__expand-card-expand-icon');
-    expandIcon.setAttribute('src', './public/services--expandcardplusicon.svg');
-    expandIcon.setAttribute('alt', '');
+        // Create the minimize icon
+        const minimizeIcon = document.createElement('img');
+        minimizeIcon.classList.add('services__expand-card-minimize-icon');
+        minimizeIcon.setAttribute('src', './public/services--expandcardminimizeicon.svg');
+        minimizeIcon.setAttribute('alt', '');
+        minimizeIcon.style.display = 'none';
 
-    // Create the minimize icon
-    const minimizeIcon = document.createElement('img');
-    minimizeIcon.classList.add('services__expand-card-minimize-icon');
-    minimizeIcon.setAttribute('src', './public/services--expandcardminimizeicon.svg');
-    minimizeIcon.setAttribute('alt', '');
-    minimizeIcon.style.display = 'none';
+        // Append the title and icons to the title container
+        titleContainer.appendChild(title);
+        titleContainer.appendChild(expandIcon);
+        titleContainer.appendChild(minimizeIcon);
 
-    // Append the title and icons to the title container
-    titleContainer.appendChild(title);
-    titleContainer.appendChild(expandIcon);
-    titleContainer.appendChild(minimizeIcon);
+        // Create the expanded text container
+        const expandedText = document.createElement('div');
+        expandedText.classList.add('services__expand-card-expanded');
 
-    // Create the expanded text container
-    const expandedText = document.createElement('div');
-    expandedText.classList.add('services__expand-card-expanded');
+        // Create the description text element
+        const description = document.createElement('p');
+        description.classList.add('services__expand-card-text', 'body-global-font');
+        description.textContent = service.serviceDescription;
 
-    // Create the description text element
-    const description = document.createElement('p');
-    description.classList.add('services__expand-card-text', 'body-global-font');
-    description.textContent = service.serviceDescription;
+        // Append the description text to the expanded text container
+        expandedText.appendChild(description);
 
-    // Append the description text to the expanded text container
-    expandedText.appendChild(description);
+        // Append the title container and expanded text to the main container
+        expandCard.appendChild(titleContainer);
+        expandCard.appendChild(expandedText);
 
-    // Append the title container and expanded text to the main container
-    expandCard.appendChild(titleContainer);
-    expandCard.appendChild(expandedText);
+        // Append the main container to the services container
+        servicesContainer.appendChild(expandCard);
 
-    // Append the main container to the services container
-    servicesContainer.appendChild(expandCard);
+        // Add event listener to the title container
+        titleContainer.addEventListener('click', () => {
+            // Check if the clicked card is already active
+            const isCardActive = activeCard === expandCard;
 
-    // Add event listener to the expand icon
-    expandIcon.addEventListener('click', () => {
-    if (activeCard) {
-        activeCard.querySelector('.services__expand-card-expand-icon').style.display = 'inline-block';
-        activeCard.querySelector('.services__expand-card-minimize-icon').style.display = 'none';
-        activeCard.querySelector('.services__expand-card-expanded').style.display = 'none';
-    }
+            // Reset the previous active card
+            if (activeCard) {
+                activeCard.querySelector('.services__expand-card-minimize-icon').style.display = 'none';
+                activeCard.querySelector('.services__expand-card-expand-icon').style.display = 'inline-block';
+                activeCard.querySelector('.services__expand-card-expanded').style.display = 'none';
+            }
 
-    expandIcon.style.display = 'none';
-    minimizeIcon.style.display = 'inline-block';
-    expandedText.style.display = 'block';
-    activeCard = expandCard;
+            // Toggle the expand/minimize icons and expanded text for the clicked card
+            minimizeIcon.style.display = isCardActive ? 'none' : 'inline-block';
+            expandIcon.style.display = isCardActive ? 'inline-block' : 'none';
+            expandedText.style.display = isCardActive ? 'none' : 'block';
+
+            // Update the activeCard variable based on the toggle
+            activeCard = isCardActive ? null : expandCard;
+
+            // Update the icon for the clicked card
+            if (!isCardActive) {
+                expandIcon.style.display = 'none';
+                minimizeIcon.style.display = 'inline-block';
+            }
+        });
     });
-
-    // Add event listener to the minimize icon
-    minimizeIcon.addEventListener('click', () => {
-    minimizeIcon.style.display = 'none';
-    expandIcon.style.display = 'inline-block';
-    expandedText.style.display = 'none';
-    activeCard = null;
-    });
-});
 }
 
+
+
+
+
+
+
+
+
 // Call the function with the servicesList
-buildServicesHTML(servicesList);
-  
-  
+buildServicesHTML(servicesList);  
