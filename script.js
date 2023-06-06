3bf1e14420a93a4e886439b7897bce59d8dca931
Pablo Calderon Bravo <pablocalderonbravo@gmail.com>
2023-06-05 21:26:11 -0600

    Modifying the navbar and footer to make them reusable

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+// _____________________________________________________________________________________________  NAVBAR
+
+
+// Adding the navbar to page
+
+const navbarPlaceholder = document.querySelector('.navbar-container');
+window.addEventListener('DOMContentLoaded', function() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'navbar.html', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            navbarPlaceholder.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+});
+
+function toggleMenu() {
+    const menuContainer = document.querySelector(".index-mobile");
+    const hamburgerButton = document.querySelector(".navbar__hamburger-menu");
+    const navbarColor = document.querySelector(".navbar");
+    const changeLogoColor = document.querySelector(".navbar__button-logo-home");
+
+    menuContainer.classList.toggle("active");
+    hamburgerButton.classList.toggle("active");
+    navbarColor.classList.toggle("active");
+    navbarPlaceholder.classList.toggle("active");
+    changeLogoColor.classList.toggle("active");
+}
+
+const indexMobileItems = document.querySelectorAll(".index-mobile__item");
+indexMobileItems.forEach(item => {
+    item.addEventListener('click', toggleMenu);
+});
+
+// _____________________________________________________________________________________________  FOOTER
+
+// Adds footer section
+
+const footerPlaceholder = document.querySelector('.footer');
+window.addEventListener('DOMContentLoaded', function() {
+    if (footerPlaceholder) {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'footer.html', true);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          footerPlaceholder.innerHTML = xhr.responseText;
+        }
+      };
+      xhr.send();
+    }
+  });
+
+
 const footerFacebookButtonImageIcon = document.getElementById(
 "footerFacebookButtonImageIcon"
 );
@@ -41,29 +95,8 @@ const observer = new IntersectionObserver(
 );
 
 for (let i = 0; i < scrollAnimElements.length; i++) {
-observer.observe(scrollAnimElements[i]);
+    observer.observe(scrollAnimElements[i]);
 }
 
 
-
-// _____________________________________________________________________________________________  NAVBAR
-
-function toggleMenu() {
-    const menuContainer = document.querySelector(".index-mobile");
-    const hamburgerButton = document.querySelector(".navbar__hamburger-menu");
-    const navbarColor = document.querySelector(".navbar");
-    const overlayColor = document.querySelector(".navbar-container");
-    const changeLogoColor = document.querySelector(".navbar__button-logo-home");
-
-    menuContainer.classList.toggle("active");
-    hamburgerButton.classList.toggle("active");
-    navbarColor.classList.toggle("active");
-    overlayColor.classList.toggle("active");
-    changeLogoColor.classList.toggle("active");
-}
-
-const indexMobileItems = document.querySelectorAll(".index-mobile__item");
-indexMobileItems.forEach(item => {
-    item.addEventListener('click', toggleMenu);
-});
 
